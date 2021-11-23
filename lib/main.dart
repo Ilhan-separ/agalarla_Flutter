f31935d33d7bd33e40a582314e1c163661679e00
FlutLab.io <>
2021-11-23 12:28:00 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainWidget());
+
+class MainWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Delux Sayac!!'),
+          backgroundColor: Colors.red.shade100,
+          foregroundColor: Colors.red,
+        ),
+        body: Column(
+          children: [
+            Text('deneme1'),
+            Text('deneme2'),
+            Text('deneme3'),
+            ButtonBar(
+              children: [
+                RaisedButton(
+                  child: Text('aga buton 1'),
+                  onLongPress: () => print('aga uzun bastin!'),
+                  onPressed: () => print('aga normal bastin!'),
+                ),
+                RaisedButton(
+                  child: Text('aga buton 2'),
+                  onLongPress: () => print('aga uzun bastin!'),
+                  onPressed: () => print('aga normal bastin!'),
+                ),
+                ToggleButtons(
+                  isSelected: [
+                    true
+                  ],
+                  children: [
+                    Text('aga buton 3'),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
