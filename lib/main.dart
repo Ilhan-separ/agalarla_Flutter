941b401041cc8c1768cddde0b586418eacb67783
FlutLab.io <>
2021-11-25 13:39:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MainWidgetState extends State<MainWidget> {
     'DELUX 3XL Sayaç'
   ];
 
-  String selectedName;
+  String selectedName = 'defult name';
 
   void SayacIsmiDegis() {
     setState(() {
